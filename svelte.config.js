f32ca3f088f29af2f3cad1152b736c68b5ba23a8
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-02 14:18:57 +0900

    format

--- svelte.config.js
+++ svelte.config.js
@@ -11,8 +11,8 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter()
-  }
+    adapter: adapter(),
+  },
 };
 
 export default config;
@@ -36,10 +36,10 @@ export default config;
 //       // See below for an explanation of these options
 //       routes: {
 //         include: ['/*'],
-//         exclude: ['<all>']
-//       }
-//     })
-//   }
+//         exclude: ['<all>'],
+//       },
+//     }),
+//   },
 // };
 
 // export default config;
