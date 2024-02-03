84e4d3700c82e907e670a4caebf1fab99ac547ce
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-04 03:32:29 +0900

    戻った

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,32 +1,21 @@
 import { writable, derived } from "svelte/store";
-import type { schema, langs } from "../locales/$schema.ts";
+import type { langs, schema } from "../locales/$schema.ts";
 
-//import en from "../locales/en.ts";
-//import ja from "../locales/ja.ts";
+import en from "../locales/en.ts";
+import ja from "../locales/ja.ts";
 
-/*
 const locales = {
   en: en,
   ja: ja,
 };
-*/
+type locales = keyof typeof locales;
 
 export const lang = writable<langs>("en");
 
-let locale:schema;
-
 export const t = derived(
   lang,
-  ($lang) => {
-    (async () => {locale = await import("../locales/"+$lang+".ts");});
-    return (msg) => {
-      return locale[msg];
-    };
-  },
+  ($lang: langs) =>
+    (msg: keyof locales[langs]) => {
+      return locales[$lang][msg];
+    },
 );
-
-/*
-(msg: keyof typeof locales[langs]) => {
-  return locales[$lang][msg];
-},
-*/
