cfa0ced028a354dd2ebfdc566298d06aab14de90
-akku- <151517253+akku1139@users.noreply.github.com>
2024-02-04 14:21:23 +0900

    圧倒的勘違い

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,5 +6,5 @@ import { t, lang } from "$lib/i18n.ts";
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-{ t("langname") }
+{ $t("langname") }
 <button type="button" on:click='{()=>{lang.set($lang=="en"?"ja":"en");}}'>Switch lang (en/ja)</button>
